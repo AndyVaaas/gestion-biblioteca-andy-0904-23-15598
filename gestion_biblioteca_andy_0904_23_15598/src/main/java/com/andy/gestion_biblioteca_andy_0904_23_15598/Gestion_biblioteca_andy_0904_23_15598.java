a41de68010459332512064f58ebf62ffc3794c3a
umg <umg@HUE-LAB-1C4.labhue.local>
2026-04-25 10:14:42 -0600

    He creado el proyecto y su estructura de paquetes

---
+++ gestion_biblioteca_andy_0904_23_15598/src/main/java/com/andy/gestion_biblioteca_andy_0904_23_15598/Gestion_biblioteca_andy_0904_23_15598.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.andy.gestion_biblioteca_andy_0904_23_15598;
+
+/**
+ *
+ * @author umg
+ */
+public class Gestion_biblioteca_andy_0904_23_15598 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
